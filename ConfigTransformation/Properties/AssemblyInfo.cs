bb219c078af4fa4f461c28f19e71169d5d89dc67
marazt <marazt@gmail.com>
2014-07-17 22:04:39 +0200

    - Added new build definition Deploy
    - Added post-build script to obfuscate dlls and copy them into final vsix archive

--- ConfigTransformation/Properties/AssemblyInfo.cs
+++ ConfigTransformation/Properties/AssemblyInfo.cs
@@ -29,8 +29,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.3.14198.1551")]
-[assembly: AssemblyFileVersion("1.3.14198.1551")]
+[assembly: AssemblyVersion("1.3.14198.2000")]
+[assembly: AssemblyFileVersion("1.3.14198.2000")]
 
 [assembly: InternalsVisibleTo("ConfigTransformation_IntegrationTests, PublicKey=002400000480000094000000060200000024000052534131000400000100010007d1487f7a52164dba1f647980bbae24a993982e5341842b134389139d8b1ff013c7bcdfc4b7cf82ee2eccddf0740d97dbb41eb01f9cb591219530f0b3ae6ca65d9268f9e5ac214fc49b74a5532755444d026e509b8ac645264910228ad355c94619679467ca8a612609b6fc01a96b2cc89fb501e86560daff48426bfc764c9f")]
 [assembly: InternalsVisibleTo("ConfigTransformation_UnitTests, PublicKey=002400000480000094000000060200000024000052534131000400000100010007d1487f7a52164dba1f647980bbae24a993982e5341842b134389139d8b1ff013c7bcdfc4b7cf82ee2eccddf0740d97dbb41eb01f9cb591219530f0b3ae6ca65d9268f9e5ac214fc49b74a5532755444d026e509b8ac645264910228ad355c94619679467ca8a612609b6fc01a96b2cc89fb501e86560daff48426bfc764c9f")]
